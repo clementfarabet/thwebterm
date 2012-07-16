703143d5f5adc487ff61adcc111282a8ded4b63b
Clement Farabet <clement.farabet@gmail.com>
2012-07-15 23:52:03 -0400

    Added legends

--- tests/test_plots.lua
+++ tests/test_plots.lua
@@ -1,6 +1,6 @@
 
 plot1 = sin(linspace(-10,10,500))
-plot2 = tanh(linspace(-10,10,500))
+plot2 = tanh(linspace(-10,10,500)):pow(2)
 
-plot(plot1)
-plot(plot2)
+plot(plot1) print('Figure 1. $sin(X)$')
+plot(plot2) print('Figure 2. $tanh(X)^2$')
